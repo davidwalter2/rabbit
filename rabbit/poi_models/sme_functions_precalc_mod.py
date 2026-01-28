822c3b3ea29fe1c9184077f770ae3204e008eac5
benkjor <jorian.benke@gmail.com>
2026-01-28 12:16:51 -0500

    save by Q2 value

--- rabbit/poi_models/sme_functions_precalc_mod.py
+++ rabbit/poi_models/sme_functions_precalc_mod.py
@@ -158,34 +158,8 @@ def summation_terms(Q2, e_f, g):
 
 
 
-def sigma_hat_prime(x, tau, C, p1, p2, flavor, Q2):
-    # try:
-    tau_x = tau/x
-    
-    f_s_val = f_s(x, tau, flavor, Q2)
-    f_prime_s_val = f_prime_s(x, tau, flavor, Q2)
-        
-        ### need to save this
-    # Efficiently handle the contraction with non-zero elements of C
-    contraction_p1p1 = tn.einsum('mn,m,n->', C, p1, p1)
-    contraction_p1p2 = tn.einsum('mn,m,n->', C, p1, p2)
-    contraction_p2p1 = tn.einsum('mn,m,n->', C, p2, p1)
-    contraction_p2p2 = tn.einsum('mn,m,n->', C, p2, p2)
-    
-    term1 = f_s_val
-        
-    # term2 = 2* (1 + x / tau_x) * f_s_val # (contraction_p1p1 + contraction_p1p2 +  contraction_p2p1 + contraction_p2p2)
-    term3 = 2 * (x * contraction_p1p1 +  tau_x * contraction_p1p2 + tau_x * contraction_p2p1 + x * contraction_p2p2) * f_prime_s_val
-
-    # return term1, term2 + term3
-    return term1, term3
-
-
 def integrate_sigma_hat_prime_sme(tau, flavor, Q2):
-    # def integrand1(x):
-    #     tau_x = tau / x
-    #     term1 = 2*f_s(x, tau, flavor, Q2)
-    #     return term1 * tau_x
+
     def integrand2(x):
         tau_x = tau / x
         term2 = 2*f_s(x, tau, flavor, Q2) + 2* (x**2/tau) * f_s(x, tau, flavor, Q2)
@@ -199,29 +173,6 @@ def integrate_sigma_hat_prime_sme(tau, flavor, Q2):
         term4 = 2* tau_x * f_prime_s(x, tau, flavor, Q2)
         return term4 * tau_x
     
-    
-    ### don't want to use this, it produces a value that is off in the 
-    # def integrandA(x):
-    #     # (result2 + 2* result1 + result3)
-    #     tau_x = tau / x
-    #     term2 = 2* (x**2/tau) * f_s(x, tau, flavor, Q2)
-    #     term1 = 2 * f_s(x, tau, flavor, Q2)
-    #     term3 = 2* x * f_prime_s(x, tau, flavor, Q2)
-    #     return (term1 + term2 + term3) * tau_x
-    
-    # def integrandB(x):
-    # # (result4 + result2 + 2* result1)
-    #     tau_x = tau / x
-    #     term4 = 2* tau_x * f_prime_s(x, tau, flavor, Q2)
-    #     term2 = 2* (x**2/tau) * f_s(x, tau, flavor, Q2)
-    #     term1 = 2 * f_s(x, tau, flavor, Q2)
-    #     return (term1 + term2 + term4) * tau_x
-        
-    # resultA, _ = quad(integrandA, tau, 1)
-    # resultB, _ = quad(integrandB, tau, 1)
-
-    # return resultA * (contraction_p1p1 + contraction_p2p2)  + resultB * (contraction_p1p2 + contraction_p2p1)
-    
         
     result2, _ = quad(integrand2, tau, 1)
     result3, _ = quad(integrand3, tau, 1)
@@ -233,66 +184,76 @@ def integrate_sigma_hat_prime_sme(tau, flavor, Q2):
     return pipi, pipj
     
     
-    
-    
-    # return (result2 + 2* result1)* (contraction_p1p1 + contraction_p1p2 +  contraction_p2p1 + contraction_p2p2) + result3 * (contraction_p1p1 + contraction_p2p2) + result4 *(contraction_p1p2 + contraction_p2p1)
 
-def d_sigma(Q2, contraction_pipi_L, contraction_pipj_L, contraction_pipi_R, contraction_pipj_R, quark_couplings):
+
+def d_sigma(Q2, p1, p2, quark_couplings, precomputed = False, Q_min = 0, Q_max = 0, num_steps = 100):
     tau = Q2 / s
     d_sigmaL = 0
     d_sigmaR = 0
     i = 0
     
-    for flavor, e_f, g_fR, g_fL in quark_couplings:
-        pipi_L, pipj_L = integrate_sigma_hat_prime_sme(tau, flavor, Q2)
+    for flavor, e_f, g_fR, g_fL, CL, CR in quark_couplings:
+        if precomputed:
+            filename = f"precomputed_sme/{Q_min}_to_{Q_max}_{Q2}GeV_{flavor}_{num_steps}.pkl"
+            try:
+                with open(filename, "rb") as f:
+                    combined_array = pickle.load(f)
+                    pipi_L, pipj_L, sum_terms_L, pipi_R, pipj_R, sum_terms_R = combined_array
+            except: 
+                precomputed = False
+        if not precomputed:
+            pipi_L, pipj_L = integrate_sigma_hat_prime_sme(tau, flavor, Q2)
+            pipi_R, pipj_R = integrate_sigma_hat_prime_sme(tau, flavor, Q2)
+            
+            sum_terms_L = summation_terms(Q2, e_f, g_fL)
+            sum_terms_R = summation_terms(Q2, e_f, g_fR)
         
+            
+            filename = f"precomputed_sme/{Q_min}_to_{Q_max}_{Q2}GeV^2_{quarks[flavor-1]}_{num_steps}_steps.pkl"
+            ## store left and right in the same file
+            combined_array = np.array([pipi_L,pipj_L, sum_terms_L, pipi_R, pipj_R,  sum_terms_R])
+            with open(filename, "wb") as f:
+                pickle.dump(combined_array, f)
+                
+        contraction_p1p1_L = tn.einsum('mn,m,n->', CL, p1, p1)
+        contraction_p1p2_L = tn.einsum('mn,m,n->', CL, p1, p2)
+        contraction_p2p1_L = tn.einsum('mn,m,n->', CL, p2, p1)
+        contraction_p2p2_L = tn.einsum('mn,m,n->', CL, p2, p2)
+        
+        contraction_pipi_L = (contraction_p1p1_L + contraction_p2p2_L)
+        contraction_pipj_L = (contraction_p1p2_L + contraction_p2p1_L)
+        
+        
+        contraction_p1p1_R = tn.einsum('mn,m,n->', CR, p1, p1)
+        contraction_p1p2_R = tn.einsum('mn,m,n->', CR, p1, p2)
+        contraction_p2p1_R = tn.einsum('mn,m,n->', CR, p2, p1)
+        contraction_p2p2_R = tn.einsum('mn,m,n->', CR, p2, p2)
+
+        contraction_pipi_R = (contraction_p1p1_R + contraction_p2p2_R)
+        contraction_pipj_R = (contraction_p1p2_R + contraction_p2p1_R)
+
         integral1 = pipi_L * contraction_pipi_L + pipj_L* contraction_pipj_L
-        
-        pipi_R, pipj_R = integrate_sigma_hat_prime_sme(tau, flavor, Q2)
-        
         integral2 = pipi_R * contraction_pipi_R + pipj_R* contraction_pipj_R
 
-        sum_terms_L = summation_terms(Q2, e_f, g_fL)
-        sum_terms_R = summation_terms(Q2, e_f, g_fR)
 
         d_sigmaL +=  sum_terms_L * integral1
         d_sigmaR += sum_terms_R * integral2
         
+
         i += 1
 
     return factor * 0.389379 * 1e9*(d_sigmaL + d_sigmaR)  # This is d\sigma / dQ^2
 
 
 
-def sme(Q_min, Q_max, CL, CR, p1, p2, quark_couplings, num_steps_Q2 =100,):
+def sme(Q_min, Q_max, p1, p2, quark_couplings, num_steps_Q2 = 100, precomputed = False):
+
     num_steps_Q2 = int(num_steps_Q2)
-    # Ensure that num_steps is odd for Simpson's rule to work properly
     if num_steps_Q2 % 2 == 0:
         num_steps_Q2 += 1
     Q2_values = np.linspace(Q_min**2, Q_max**2, num_steps_Q2)
-    
-    # Loop over Q2 values and calculate d_sigma for each
-    
-    
-    contraction_p1p1_L = tn.einsum('mn,m,n->', CL, p1, p1)
-    contraction_p1p2_L = tn.einsum('mn,m,n->', CL, p1, p2)
-    contraction_p2p1_L = tn.einsum('mn,m,n->', CL, p2, p1)
-    contraction_p2p2_L = tn.einsum('mn,m,n->', CL, p2, p2)
-    
-    contraction_pipi_L = (contraction_p1p1_L + contraction_p2p2_L)
-    contraction_pipj_L = (contraction_p1p2_L + contraction_p2p1_L)
-       
-    
-    contraction_p1p1_R = tn.einsum('mn,m,n->', CR, p1, p1)
-    contraction_p1p2_R = tn.einsum('mn,m,n->', CR, p1, p2)
-    contraction_p2p1_R = tn.einsum('mn,m,n->', CR, p2, p1)
-    contraction_p2p2_R = tn.einsum('mn,m,n->', CR, p2, p2)
-    
 
-    contraction_pipi_R = (contraction_p1p1_R + contraction_p2p2_R)
-    contraction_pipj_R = (contraction_p1p2_R + contraction_p2p1_R)
-    
-    integrand_values = np.array([d_sigma(Q2, contraction_pipi_L, contraction_pipj_L, contraction_pipi_R, contraction_pipj_R, quark_couplings) for Q2 in Q2_values])
+    integrand_values = np.array([d_sigma(Q2, p1, p2, quark_couplings,precomputed, Q_min, Q_max) for Q2 in Q2_values])
     
     # Use Simpson's rule to integrate over the Q2_values array
     integral_liv = simpson(integrand_values, Q2_values)
@@ -300,105 +261,13 @@ def sme(Q_min, Q_max, CL, CR, p1, p2, quark_couplings, num_steps_Q2 =100,):
     return integral_liv
 
 
-
-
-
-'''
-
-#STANDARD MODEL EXTENSION
-
-def int_A(x, tau, flavor, Q2):
-    return 2/s *(1 + x**2/tau) * f_s(x, tau, flavor, Q2)
-
-def int_B(x, tau, flavor, Q2):
-    return 2/s  * f_prime_s(x, tau, flavor, Q2)
-    
-    
-def int_A_Bx(tau, flavor, Q2):
-    def integrand(x):
-        tau_x = tau / x
-        A = int_A(x, tau, flavor, Q2)
-        B = int_B(x, tau, flavor, Q2)
-        return tau_x * (A + B*x)
-
-    result2, _ = quad(integrand, tau, 1)
-    return result2
-
-
-def int_A_B_taux(tau, flavor, Q2):
-    def integrand(x):
-        tau_x = tau / x
-        A = int_A(x, tau, flavor, Q2)
-        B = int_B(x, tau, flavor, Q2)
-        return tau_x * (A + B*tau_x)
-
-    result2, _ = quad(integrand, tau, 1)
-    return result2
-
-
-def int_tau_x(tau, flavor, Q2):
-    def integrand(x):
-        tau_x = tau / x
-        return tau_x * f_s(x, tau, flavor, Q2)
-    result2, _ = quad(integrand, tau, 1)
-    return result2
-    
-
-
-def d_sigma(Q2, CL, CR, p1, p2, quark_couplings,):
-    tau = Q2 / s
-
-    d_sigma = 0
-    i = 0
-    for flavor, e_f, g_fR, g_fL, wilson_coeff_L, wilson_coeff_R in quark_couplings:
-        # pdb.set_trace()
-        C_fL = CL
-        C_fR = CR
-        ## these are right
-        prefactorL = summation_terms(Q2, e_f, g_fL) 
-        prefactorR = summation_terms(Q2, e_f, g_fR) 
-        
-        i += 1
-        
-        ## these will be the same
-        contraction_p1p1_left = tn.einsum('mn,m,n->', C_fL, p1, p1)
-        contraction_p1p2_left = tn.einsum('mn,m,n->', C_fL, p1, p2)
-        contraction_p2p1_left = tn.einsum('mn,m,n->', C_fL, p2, p1)
-        contraction_p2p2_left = tn.einsum('mn,m,n->', C_fL, p2, p2)
-        
-        
-        d_sigmafL = (contraction_p1p1_left + contraction_p2p2_left)* int_A_Bx(tau, flavor, Q2) + (contraction_p1p2_left + contraction_p2p1_left) * int_A_B_taux(tau, flavor, Q2)
-        
-        
-        contraction_p1p1_right = tn.einsum('mn,m,n->', C_fR, p1, p1)
-        contraction_p1p2_right = tn.einsum('mn,m,n->', C_fR, p1, p2)
-        contraction_p2p1_right = tn.einsum('mn,m,n->', C_fR, p2, p1)
-        contraction_p2p2_right = tn.einsum('mn,m,n->', C_fR, p2, p2)
-        d_sigmafR = (contraction_p1p1_right + contraction_p2p2_right)* int_A_Bx(tau, flavor, Q2) + (contraction_p1p2_right + contraction_p2p1_right) * int_A_B_taux(tau, flavor, Q2)
-        
-        sm_term = int_tau_x(tau, flavor, Q2)
-        d_sigma += (d_sigmafL * prefactorL)+ (d_sigmafR*prefactorR) + sm_term*(prefactorL + prefactorR)
-        print(d_sigmafL * prefactorL + sm_term*prefactorL)
-    return factor *0.389379 * 1e9*(d_sigma) 
-
-
-def sme(Q_min, Q_max, CL, CR, p1, p2, quark_couplings, num_steps_Q2 =100,):
-    num_steps_Q2 = int(num_steps_Q2)
-    # Ensure that num_steps is odd for Simpson's rule to work properly
-    if num_steps_Q2 % 2 == 0:
-        num_steps_Q2 += 1
-    Q2_values = np.linspace(Q_min**2, Q_max**2, num_steps_Q2)
-    
-    # Loop over Q2 values and calculate d_sigma for each
-    integrand_values = np.array([d_sigma(Q2, CL, CR, p1, p2, quark_couplings) for Q2 in Q2_values])
-    
-    # Use Simpson's rule to integrate over the Q2_values array
-    integral_liv = simpson(integrand_values, Q2_values)
-    print(integral_liv)
-    return integral_liv
-'''
 
 times, pm, pn = get_hour_array() ## i only want to compte this once when i do the 
 t = time.time()
-sme(70, 80, CL4*1e-5, CR, pm[0], pn[0], quark_couplings)
+# sme(70, 80, pm[0], pn[0], wilson_couplings)
+
+mybins = [15, 25, 28, 30, 32, 34, 36, 38, 40, 42, 47, 55, 60, 65, 80]
+# for i in range(len(mybins) - 1):
+for i in range(0, 1):
+    sme(mybins[i], mybins[i+1], pm[0], pn[0], wilson_couplings)
 print(t - time.time())
