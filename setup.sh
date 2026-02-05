93f069dad16003109c300dde1dd76f17e9251ff9
davidwalter2 <david.walter@cern.ch>
2026-02-05 13:47:42 -0500

    Add flag to ensure numerical reproducebility

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+export TF_ENABLE_ONEDNN_OPTS=0 
 export RABBIT_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PYTHONPATH="${RABBIT_BASE}:$PYTHONPATH"
 export PATH="$PATH:${RABBIT_BASE}/bin"
