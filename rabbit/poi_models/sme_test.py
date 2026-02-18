d10cb2f109493ebd8aca4953c51a3e95308fc125
benkjor <jorian.benke@gmail.com>
2026-02-18 02:55:24 -0500

    plot the sinusoid

--- rabbit/poi_models/sme_test.py
+++ rabbit/poi_models/sme_test.py
@@ -3,18 +3,15 @@ import tensorflow as tf
 from rabbit.poi_models.sme_constants import *
 from rabbit.poi_models.sme_functions import *
 
-import h5py
-from utilities.io_tools import input_tools
 import pdb
 from scripts.plotting.uncertainty_tools import *
 from rabbit.poi_models.poi_model import POIModel
 import tensorflow as tf
 
-import tensorflow as tf
-
+import matplotlib.pyplot as plt
 
 # cxx_val = 1e-4*0.906
-cxx_val = 1e-5
+cxx_val = 66e-6
 
 
 # for channel, info in indata.channel_info.items():
@@ -51,3 +48,11 @@ sm_sigma = tf.cast([precomp_dict["values"][9]]*nTimeBins, dtype = tf.float64) ##
         
 flattened_xsec = (sm_sigma + sme_left*cxx_val + sme_right * 0)/sm_sigma
 print(flattened_xsec)
+times = np.linspace(0, 24, 24)
+plt.step(times, flattened_xsec, where = "post")
+outdir = "/home/submit/jbenke/public_html/wilson_coeff_fits/"
+date = "2026-02-17/"
+plt.ylim([0.99, 1.01])
+plt.title(f"WC = {cxx_val}")
+filename = f"sigma_sme_sigma_sm_{cxx_val}"
+plt.savefig(outdir + date + filename, format = "png")
